4783478b346256ef3784c0b0f687b76916eb3ccf
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-05-20 23:27:28 -0400

    bug fixing

--- SpringAssignmentTask1/src/com/spring/assignmenttask1/DriverClass.java
+++ SpringAssignmentTask1/src/com/spring/assignmenttask1/DriverClass.java
@@ -7,7 +7,7 @@ public class DriverClass {
 	public static void main(String[] args) {
 		 
 		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("Applicationcontext.xml");
+				new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		
 		JavaLanguage language = context.getBean("Framework", JavaLanguage.class);
 		
